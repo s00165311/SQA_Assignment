4906314600a04f6b77e0c71848eaffe23632710b
mark o shea <s00015630@mail.itsligo.ie>
2018-02-21 16:46:56 +0000

    fitness added

--- SqaAssignment/CalculatePremium.cs
+++ SqaAssignment/CalculatePremium.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using fit;
+using fit.Fixtures;
 namespace SqaAssignment
 {
     public class CalculatePremium
@@ -15,28 +16,42 @@ namespace SqaAssignment
             float premium;
 
             if (gender == "female")
-	if ((age >= 18) && (age <= 30))
-                premium = 5.0F;
-            else
-        if (age >= 31)
-                premium = 2.50F;
-            else
-                premium = 0.0F;
-else
-	if (gender == "male")
-	    if ((age >= 18) && (age <= 35))
-                premium = 6.0F;
-            else
-           if (age >= 36)
-                premium = 5.0F;
-            else
-                premium = 0.0F;
-else
-		premium = 0.0F;
+	            if ((age >= 18) && (age <= 30))
+                            premium = 5.0F;
+                        else
+                    if (age >= 31)
+                            premium = 2.50F;
+                        else
+                            premium = 0.0F;
+         else
+	        if (gender == "male")
+	            if ((age >= 18) && (age <= 35))
+                        premium = 6.0F;
+                    else
+                   if (age >= 36)
+                        premium = 5.0F;
+                    else
+                        premium = 0.0F;
+         else
+		        premium = 0.0F;
 
-            if (age >= 50)
-                premium = premium * 0.15F;
-            return premium;
+                if (age >= 50)
+                    premium = premium * 0.15F;
+                return premium;
+        }
+        public class CalculatePremiumTest : ColumnFixture
+        {
+            public string gender;
+            public int age;
+            public float premium;
+
+            public CalculatePremium cal = new CalculatePremium();
+            
+            public float CalcPremium()
+            {
+                return cal.CalcPremium(age, gender);
+            }
+
         }
 
     }
